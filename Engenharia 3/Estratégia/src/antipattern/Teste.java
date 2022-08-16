75a63fd240640bacc22566d366f1b031820d0d69
Gustavo Ando <79657871+GustavoAndo@users.noreply.github.com>
2022-08-16 16:59:31 -0300

    Add files via upload

---
+++ Engenharia 3/Estratégia/src/antipattern/Teste.java
@@ -0,0 +1,28 @@
+package antipattern;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		Equipamento espada = new Espada();
+		espada.equipar();
+		System.out.println("--------------------------------");
+		
+		Equipamento escudo = new Escudo();
+		escudo.equipar();
+		
+		System.out.println("--------------------------------");
+		
+		Equipamento machado = new Machado();
+		machado.equipar();
+		
+		System.out.println("--------------------------------");
+		
+		Equipamento maca = new Maca();
+		maca.equipar();
+		
+		System.out.println("--------------------------------");
+			
+	}
+	
+}
